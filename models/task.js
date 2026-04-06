60fb13d6bb62dbbf0c392548b66c75113d0d1c24
Onyinyechi Rita Ngaokere <ngaokere.o.rita@gmail.com>
2026-04-06 14:21:46 -0400

    fix neon ssl

--- models/task.js
+++ models/task.js
@@ -5,7 +5,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   protocol: "postgres",
   logging: false,
   dialectOptions: {
-    ssl: false
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 });
 
